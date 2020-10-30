268d25fa25e076da855a49be328723b0691244d7
Brian Kubes <briankubes@gmail.com>
2020-10-30 09:26:53 -0700

    all crud works for projectRouter.js

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -3,7 +3,45 @@ const router = express.Router()
 const Project = require('./projectModel.js')
 
 router.get('/', (req, res)=> {
-    console.log('pRouter working')
+    Project.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message })
+    })
 })
+
+router.delete('/:id', (req, res)=> {
+    Project.remove(req.params.id)
+    .then(project => {
+        res.status(200).json({message: 'project was deleted'})
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message })
+    })
+})
+
+router.post('/', (req, res)=> {
+    Project.insert(req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message })
+    })
+})
+
+router.put('/:id', (req, res)=> {
+    Project.update(req.params.id, req.body)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message })
+    })
+})
+
+
 
 module.exports = router
